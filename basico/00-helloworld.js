a16b4e164447988b361792a382f18eb43ec6e9d2
Alexis Jacob <ferjacobka@gmail.com>
2026-05-07 12:40:31 -0400

    primer commit js

---
+++ basico/00-helloworld.js
@@ -0,0 +1,20 @@
+// primer comentario de mi hello world
+
+/* esto 
+me 
+permite
+poner 
+varias 
+lineas de comentarios
+*/
+
+console.log("Hello World")
+console.log("alterado")
+console.log("2")
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 / 2)
+console.log(2 % 2)
+console.log(2 ** 2)
+
