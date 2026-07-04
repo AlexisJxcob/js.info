21ad6685b80c3b1dc28f084a730b0ccbfc1216b6
Alexis Jacob <ferjacobka@gmail.com>
2026-07-04 19:30:36 -0400

    empezando con news

---
+++ 3-js-object/3.4-news/3.4-news.js
@@ -0,0 +1,8 @@
+// Constructor, operador "new"
+// El sintaxis habitual {...} nos permite crear un objeto. Pero a menudo necesitamos crear varios objetos similares, como múltiples usuarios, elementos de menú.
+
+// Función constructora
+// La función constructora es técnicamente una función normal. Aunque hay dos convenciones:
+
+// Son nombradas con la primera letra mayúscula.
+// Sólo deben ejecutarse con el operador "new".
